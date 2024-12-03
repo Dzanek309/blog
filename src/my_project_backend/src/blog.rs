891ae20e541b1bf9d158aef16e6d0b2374a0c3bb
Jan Bil <64773378+Dzanek309@users.noreply.github.com>
2024-12-03 16:37:03 +0000

    added blogs

--- src/my_project_backend/src/blog.rs
+++ src/my_project_backend/src/blog.rs
@@ -1,8 +1,23 @@
-struct Blog{
+use candid::CandidType;
+
+#[derive(Clone, CandidType)]
+pub struct Blog {
     title: String,
-    date: u32
+    date: u32,
+    content: String,
+    tags: Vec<String>
 }
 
-// u32 - liczba nie ujemna która spełnia
+impl Blog {
+    pub fn new(title: String, date: u32, content: String, tags: Vec<String>) -> Self {
+        Self {
+            title,
+            date,
+            content,
+            tags
+        }
+    }
+}
+// u32 - liczba nie ujemna która spełnia pewną zasadnę np.
 // typ "u" to inaczej unsigned ,czyli nie ujemny
 // u8 - liczba z zakresu od 0 do 255 (256 - 1)

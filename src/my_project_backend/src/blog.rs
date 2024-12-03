eacae49cee950bfac0c8a27f46a31dc3dab009a7
Jan Bil <janbil9991@gmail.com>
2024-12-03 15:37:27 +0000

    in progress to add blogs

---
+++ src/my_project_backend/src/blog.rs
@@ -0,0 +1,8 @@
+struct Blog{
+    title: String,
+    date: u32
+}
+
+// u32 - liczba nie ujemna która spełnia
+// typ "u" to inaczej unsigned ,czyli nie ujemny
+// u8 - liczba z zakresu od 0 do 255 (256 - 1)

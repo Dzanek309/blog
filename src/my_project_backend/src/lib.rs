eda4a827c47bca24826411b9982f2972d3cb713e
Jan Bil <64773378+Dzanek309@users.noreply.github.com>
2024-12-03 17:39:03 +0000

    check

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -11,9 +11,19 @@ thread_local!{
 //Tag
 
 #[ic_cdk::update]
-fn add_blog(title: String, content: String, tags: Vec<String>){
+// zadanie domowe
+//content ma być mniejszy od 500 znaków,a vector tagów ma być mniejszy lub równy 3 tagi
+// content  <= 500
+// tags <= 3 tagi
+fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog,String>{
+    if title.len() > 250{
+        return Err("Title is too long!".to_string())
+    }
+    // assert!(title.len() <= 250, "title is too long");
     let blog = Blog::new(title,content,tags);
     BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
+    let last_blog = BLOGS.with(|blogs| blogs.borrow().last().expect("Vec should not be empty").clone());
+    Ok(last_blog)
 }
 
 #[ic_cdk::query]
